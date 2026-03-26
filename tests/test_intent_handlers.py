80dcc98923f0d32a07c3a992c71c209f98b34a3f
Michael Hansen <mike@rhasspy.org>
2026-03-26 11:32:04 -0500

    Adding more tests

--- tests/test_intent_handlers.py
+++ tests/test_intent_handlers.py
@@ -2,7 +2,7 @@
 
 from collections.abc import Awaitable, Callable
 from functools import partial
-from typing import Dict, Tuple, Any
+from typing import Any, Dict, Tuple
 from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
@@ -10,8 +10,8 @@ from hassil import SlotList, TextSlotList
 
 from default_agent.agent import async_converse as agent_async_converse
 from default_agent.hass_api import HomeAssistant, InfoForRecognition
-from default_agent.models import Area, Floor, Entity, State
 from default_agent.intents_loader import IntentsLoader
+from default_agent.models import Area, Entity, Floor, LanguageIntents, State
 
 from .conftest import TEST_DATETIME
 
@@ -24,8 +24,8 @@ def to_state(entity: Entity, state: Any) -> State:
     )
 
 
-@pytest.fixture(name="hass", scope="session")
-def hass_fixture() -> MagicMock:
+@pytest.fixture(name="hass_info", scope="session")
+def hass_info_fixture() -> InfoForRecognition:
     current_floor = Floor("current-floor", "Current Floor")
     current_area = Area("current-area", "Current Area", floor_id=current_floor.floor_id)
     other_area = Area("other-area", "Other Area")
@@ -56,7 +56,7 @@ def hass_fixture() -> MagicMock:
         ),
     }
 
-    hass_info = InfoForRecognition(
+    return InfoForRecognition(
         slot_lists=slot_lists,
         preferred_area_id=current_area.area_id,
         preferred_area_name=current_area.name,
@@ -66,18 +66,31 @@ def hass_fixture() -> MagicMock:
         areas={a.area_id: a for a in areas},
         floors={f.floor_id: f for f in floors},
     )
+
+
+@pytest.fixture(name="intents_loader", scope="session")
+def intents_loader_fixture() -> IntentsLoader:
+    return IntentsLoader()
+
+
+@pytest.fixture(name="lang_intents", scope="session")
+def lang_intents_fixture(intents_loader: IntentsLoader) -> LanguageIntents:
+    lang_intents = intents_loader.get_intents("en")
+    assert lang_intents is not None, "No English intents"
+    return lang_intents
+
+
+@pytest.fixture(name="hass")
+def hass_fixture(hass_info: InfoForRecognition) -> MagicMock:
     hass = AsyncMock()
     hass.get_info.return_value = hass_info
-
     return hass
 
 
-@pytest.fixture(name="async_converse", scope="session")
-def async_converse_fixture(hass: HomeAssistant) -> CONVERSE_TYPE:
-    intents_loader = IntentsLoader()
-    lang_intents = intents_loader.get_intents("en")
-    assert lang_intents is not None, "No English intents"
-
+@pytest.fixture(name="async_converse")
+def async_converse_fixture(
+    hass: HomeAssistant, lang_intents: LanguageIntents, intents_loader: IntentsLoader
+) -> CONVERSE_TYPE:
     return partial(
         agent_async_converse,
         hass=hass,
@@ -117,5 +130,20 @@ async def test_turn_on_lights_current_area(
     hass.call_service.assert_called_once_with(
         "light",
         "turn_on",
+        target={"entity_id": ["light.light_current_area"]},
+    )
+
+
+@pytest.mark.asyncio
+async def test_turn_off_lights_current_area(
+    async_converse: CONVERSE_TYPE, hass: MagicMock
+):
+    """Test HassTurnOff intent with lights in the current area."""
+    success, response = await async_converse("turn off the lights")
+    assert success, "Intent recognition failed"
+    assert response == "Turned off the lights"
+    hass.call_service.assert_called_once_with(
+        "light",
+        "turn_off",
         target={"entity_id": ["light.light_current_area"]},
     )
